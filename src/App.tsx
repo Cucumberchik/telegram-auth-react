982256c21b20394cc33f4c5392ccac79894ab5aa
Cucumberchik <durovalexei93@gmail.com>
2024-07-05 14:39:32 +0600

    fixed comment

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,23 @@
-
-import './App.css'
-import TelegramLoginButton from './components/TelegramButton';
+import "./App.css";
+import TelegramLoginButton from "./components/TelegramButton";
 
 function App() {
-  let name = "TelegramAuthBot"
-  const handleBot = (user:any) => {
+  let name = "TelegramAuthBot"; // Это имя бота который вы ранее создавали в BotFather
+  const handleBot = (user: any) => {
     console.log(user);
-    
-  }
+  };
 
   return (
-  <main>
-    <TelegramLoginButton
+    <main>
+      <TelegramLoginButton
         botName={name}
         buttonSize="large" // "large" | "medium" | "small"
         cornerRadius={3} // 0 - 20
         usePic={false} // true | false
         dataOnauth={handleBot}
       />
-  </main>
-  )
+    </main>
+  );
 }
 
-export default App
-
-
-
-
-
+export default App;
